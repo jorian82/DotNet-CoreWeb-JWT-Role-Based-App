8f7ad1470b3d550f651c79819005c6ca7bbd942f
Jorge Rivera <jorge.rivera@ssde.com.mx>
2025-01-10 20:22:00 -0600

    DNLA-01. Added PostgreSQL nuGet package to connect to local postgreSQL database. Added configuration missing packages and imports

--- Data/AppDBContext.cs
+++ Data/AppDBContext.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace LoginAPI.Data
 {
-    public class AppDBContext
+    public class AppDBContext : IdentityDbContext<IdentityUser>
     {
-        
+        public AppDBContext(DbContextOptions options) : base(options) { }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
